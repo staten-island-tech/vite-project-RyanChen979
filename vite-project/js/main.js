61c96110da06fc92e4c9de4d7bdc2e78cfb3dee5
Ryan Chen <ryanchen979@gmail.com>
2022-11-30 10:14:10 -0500

    dsgfkdhs

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,6 @@
 import "../styles/style.css";
-import javascriptLogo from "//javascript.svg";
-import { setupCounter } from "//counter.js";
+import javascriptLogo from "./javascript.svg";
+import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
