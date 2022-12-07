baaffaa0855f9d1aebd510be4fb4739463cfac3a
Ryan Chen <ryanchen979@gmail.com>
2022-12-07 10:16:21 -0500

    dgfhjk

--- vite-project/js/items.js
+++ vite-project/js/items.js
@@ -1,12 +1,79 @@
 export const restaurant = {
-  name: "Sushi Azabu",
+  name: "Sushi",
   img: "https://www.theyumlist.net/wp-content/uploads/2017/08/Sushi-Azabu.jpg",
-  status: "Michelin Star",
 };
 
-export const sushi = [
+export const sashimi = [
+  {
+    name: "Lean Blue Fin Tuna",
+    img: "https://cdn.shopify.com/s/files/1/0258/4369/5662/products/Akami_3_x1200.progressive.jpg?v=1588204293",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "Seasonal Snapper",
+    img: "https://i0.wp.com/miyu.sg/wp-content/uploads/2019/11/Screenshot_20191110-125502_Instagram_mh1573361769712.jpg?resize=750%2C500&ssl=1",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "Fresh Water Eel",
+    img: "https://ichisushi.com/wp-content/uploads/2022/04/Best-Eel-Sushi-Recipes.jpg",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "Jumbo Sweet Shrimp",
+    img: "http://www.oksfood.com/image/nigiri_amaebi.jpg",
+    price: "",
+    rating: "",
+  },
   {
     name: "uhh",
     img: "https://cdn.vox-cdn.com/thumbor/tSiMT0KCCdCL92DAl3CDiA5ijNE=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/56106963/Hamachi_Jalapeno_AZABU_style__1_.0.jpg",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "uhh",
+    img: "https://cdn.vox-cdn.com/thumbor/tSiMT0KCCdCL92DAl3CDiA5ijNE=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/56106963/Hamachi_Jalapeno_AZABU_style__1_.0.jpg",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "uhh",
+    img: "https://cdn.vox-cdn.com/thumbor/tSiMT0KCCdCL92DAl3CDiA5ijNE=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/56106963/Hamachi_Jalapeno_AZABU_style__1_.0.jpg",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "uhh",
+    img: "https://cdn.vox-cdn.com/thumbor/tSiMT0KCCdCL92DAl3CDiA5ijNE=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/56106963/Hamachi_Jalapeno_AZABU_style__1_.0.jpg",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "uhh",
+    img: "https://cdn.vox-cdn.com/thumbor/tSiMT0KCCdCL92DAl3CDiA5ijNE=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/56106963/Hamachi_Jalapeno_AZABU_style__1_.0.jpg",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "uhh",
+    img: "https://cdn.vox-cdn.com/thumbor/tSiMT0KCCdCL92DAl3CDiA5ijNE=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/56106963/Hamachi_Jalapeno_AZABU_style__1_.0.jpg",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "uhh",
+    img: "https://cdn.vox-cdn.com/thumbor/tSiMT0KCCdCL92DAl3CDiA5ijNE=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/56106963/Hamachi_Jalapeno_AZABU_style__1_.0.jpg",
+    price: "",
+    rating: "",
+  },
+  {
+    name: "uhh",
+    img: "https://cdn.vox-cdn.com/thumbor/tSiMT0KCCdCL92DAl3CDiA5ijNE=/0x0:5760x3840/1200x800/filters:focal(2420x1460:3340x2380)/cdn.vox-cdn.com/uploads/chorus_image/image/56106963/Hamachi_Jalapeno_AZABU_style__1_.0.jpg",
+    price: "",
+    rating: "",
   },
 ];
