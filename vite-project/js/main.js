cf9db4353b582de660e6d986fc0d9c842e07560f
Ryan Chen <ryanchen979@gmail.com>
2022-12-08 10:16:35 -0500

    whyuj

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,12 +1,16 @@
 import "../styles/style.css";
-// import { setupCounter } from "./counter.js";
 import { restaurant } from "./items.js";
+import { food } from "./items.js";
 
 console.log(restaurant);
-document.querySelector("#app").innerHTML = `
+console.log(food);
+food.filter((ah) => q);
+
+/* document.querySelector("#menu").innerHTML = `
   <div>
     <h1 id="title">${restaurant.name}</h1>
+    <section>
+    
+    </section>
   </div>
-`;
-
-// setupCounter(document.querySelector("#counter"));
+`; */
