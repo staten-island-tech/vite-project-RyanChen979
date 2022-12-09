4fbc047323d8f5dd18ce117d5816adc10d2b1df1
Ryan Chen <ryanchen979@gmail.com>
2022-12-08 19:37:36 -0500

    ehh

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,16 +1,22 @@
 import "../styles/style.css";
 import { restaurant } from "./items.js";
-import { food } from "./items.js";
+import { sashimi } from "./items.js";
 
 console.log(restaurant);
-console.log(food);
-food.filter((ah) => q);
+console.log(sashimi);
 
-/* document.querySelector("#menu").innerHTML = `
-  <div>
+document.querySelector("#storeName").innerHTML = `
     <h1 id="title">${restaurant.name}</h1>
-    <section>
-    
-    </section>
-  </div>
-`; */
+`;
+
+function sashimiCards() {
+  document.querySelector("#menu").innerHTML = `<h2>${sashimi.name}</h2>`;
+}
+function sashimiList() {
+  sashimi.forEach((sashimi) => {
+    console.log(`Name: ${sashimi.name}; Price: ${sashimi.price}`);
+    console.log(`${sashimi.name}`);
+  });
+  sashimiCards();
+}
+sashimiList();
