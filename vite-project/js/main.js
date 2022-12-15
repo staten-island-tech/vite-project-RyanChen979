3c4897033b651f3ac681ed737ac7f850b4ce66e9
Ryan Chen <ryanchen979@gmail.com>
2022-12-15 10:15:52 -0500

    ahh

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,29 +1,57 @@
 import "../styles/style.css";
-import { restaurant } from "./items.js";
-import { sashimi } from "./items.js";
 import { DOM } from "./dom.js";
+import { restaurant } from "./menu.js";
+import { lunch } from "./menu.js";
+import { dinner } from "./menu.js";
+import { dessert } from "./menu.js";
 
 console.log(restaurant);
-console.log(sashimi);
+console.log(lunch);
+console.log(dinner);
+console.log(dessert);
 
 document.querySelector("#storeName").innerHTML = `
     <h1 id="title">${restaurant.name}</h1>
 `;
 
-// function sashimiCards(sashimi) {
-
-// }
-
-function sashimiList() {
-  sashimi.forEach((sashimi) => {
-    console.log(`Name: ${sashimi.name}; Price: ${sashimi.price}`);
+function lunchMenu() {
+  lunch.forEach((el) => {
+    console.log(`Name: ${el.name}; Price: ${el.price}`);
     DOM.menu.insertAdjacentHTML(
       "afterbegin",
       `<div >
-      <h4 class="name">${sashimi.name}</h4>
-      <img class="food-imgs" src="${sashimi.img}"></img>
+      <h4 class="name">${el.name}</h4>
+      <img class="food-imgs" src="${el.img}"></img>
       </div>`
     );
   });
 }
-sashimiList();
+lunchMenu();
+
+function dinnerMenu() {
+  dinner.forEach((el) => {
+    console.log(`Name: ${el.name}; Price: ${el.price}`);
+    DOM.menu.insertAdjacentHTML(
+      "afterbegin",
+      `<div >
+      <h4 class="name">${el.name}</h4>
+      <img class="food-imgs" src="${el.img}"></img>
+      </div>`
+    );
+  });
+}
+dinnerMenu();
+
+function dessertMenu() {
+  dinner.forEach((el) => {
+    console.log(`Name: ${el.name}; Price: ${el.price}`);
+    DOM.menu.insertAdjacentHTML(
+      "afterbegin",
+      `<div >
+      <h4 class="name">${el.name}</h4>
+      <img class="food-imgs" src="${el.img}"></img>
+      </div>`
+    );
+  });
+}
+dessertMenu();
